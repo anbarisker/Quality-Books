e1e2de929f11a3086ff4ea0ba66c258fe2c35654
Anba Risker <anba92sin@hotmail.com>
2019-07-31 23:41:46 +1200

    Added Quality Books logo & Contact Us page.

---
+++ QualityBooks/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QualityBooks.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QualityBooks.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
